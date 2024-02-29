19b3405feca060d9b6928bb7440e03522a36cff8
KBiying <jich789@126.com>
2024-02-29 11:36:26 +0800

    fix: Get -> GET, Post->POST

--- day1-http-base/base3/main.go
+++ day1-http-base/base3/main.go
@@ -7,14 +7,13 @@ import (
 )
 
 func main() {
-	fmt.Println("aa")
-	r := gee.New()
-	fmt.Println("bb")
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	r := gee.New()
+
+	r.GET("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 	})
-	r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+	r.GET("/hello", func(w http.ResponseWriter, r *http.Request) {
 		for k, v := range r.Header {
 			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 		}
